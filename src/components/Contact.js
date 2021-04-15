c90eec966da683328a5e37536ff0660f54f98129
brandononeal <baoneal95@gmail.com>
2021-04-15 10:09:09 -0700

    contact form initial setup

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+
+const initialFormValues = {
+  name: "",
+  email: "",
+  message: "",
+};
 
 function Contact() {
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const change = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value });
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
   return (
     <div className="Contact">
       <h1>Contact</h1>
+      <p>Let's get in touch!</p>
+      <form onSubmit={submit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={formValues.name}
+          onChange={change}
+        ></input>
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          value={formValues.email}
+          onChange={change}
+        ></input>
+        <input
+          type="text"
+          name="message"
+          placeholder="Message"
+          value={formValues.message}
+          onChange={change}
+        ></input>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
