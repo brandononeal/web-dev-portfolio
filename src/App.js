74bc65a9fa1219e90367242c95f91fcf93bd5cf9
brandononeal <baoneal95@gmail.com>
2021-06-21 21:35:32 -0700

    added links to resume, github, linkedin, and project component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
-import About from "./components/About";
+import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Switch>
-        <Route path="/about" component={About} />
+        <Route path="/projects" component={Projects} />
         <Route path="/contact" component={Contact} />
         <Route path="/" component={Home} />
       </Switch>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu } from "antd";
 import {
-  HomeOutlined,
-  InfoCircleOutlined,
+  CodeOutlined,
   MailOutlined,
+  FileTextOutlined,
+  GithubOutlined,
+  LinkedinOutlined,
 } from "@ant-design/icons";
 
 function Navbar() {
@@ -16,16 +18,33 @@ function Navbar() {
 
   return (
     <div className="Navbar">
-      <h2>Brandon O'Neal</h2>
+      <h2>
+        <Link to="/">Brandon O'Neal</Link>
+      </h2>
       <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-        <Menu.Item key="home" icon={<HomeOutlined />}>
-          <Link to="/">Home</Link>
-        </Menu.Item>
-        <Menu.Item key="about" icon={<InfoCircleOutlined />}>
-          <Link to="/about">About</Link>
+        <Menu.Item key="projects" icon={<CodeOutlined />}>
+          <Link to="/projects">Projects</Link>
         </Menu.Item>
         <Menu.Item key="contact" icon={<MailOutlined />}>
           <Link to="/contact">Contact</Link>
+        </Menu.Item>
+        <Menu.Item key="resume" icon={<FileTextOutlined />}>
+          <a
+            href="https://docs.google.com/document/d/1DV-lUf8bv02Be29jFoH-4b_TUGkMLsYCSMpRE5BTtvQ/edit"
+            target="blank"
+          >
+            Resume
+          </a>
+        </Menu.Item>
+        <Menu.Item key="github" icon={<GithubOutlined />}>
+          <a href="https://www.github.com/brandononeal" target="blank">
+            GitHub
+          </a>
+        </Menu.Item>
+        <Menu.Item key="linkedin" icon={<LinkedinOutlined />}>
+          <a href="https://www.linkedin.com/in/brandonaoneal" target="blank">
+            LinkedIn
+          </a>
         </Menu.Item>
       </Menu>
     </div>
