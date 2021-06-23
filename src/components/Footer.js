2b55754cd00bafb7be0168c4a6f05bf298ef62bb
brandononeal <baoneal95@gmail.com>
2021-06-22 18:09:01 -0700

    footer wip

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,23 @@ import React from "react";
 function Footer() {
   return (
     <div className="Footer">
-      <h1>Footer</h1>
+      <div className="footer-left">
+        <h2>made by brandon</h2>
+      </div>
+      <div className="footer-right">
+        <a
+          href="https://docs.google.com/document/d/1DV-lUf8bv02Be29jFoH-4b_TUGkMLsYCSMpRE5BTtvQ/edit"
+          target="blank"
+        >
+          Resume
+        </a>
+        <a href="https://www.github.com/brandononeal" target="blank">
+          GitHub
+        </a>
+        <a href="https://www.linkedin.com/in/brandonaoneal" target="blank">
+          LinkedIn
+        </a>
+      </div>
     </div>
   );
 }
