c130e4a6ab688d4d6e79dff0cad5d515ed87b15d
brandononeal <baoneal95@gmail.com>
2021-07-07 17:26:04 -0700

    updating project links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,18 +13,18 @@ function Projects() {
         <h2>Family Promise: Shelter Management System</h2>
         <h3>Design Lead - React</h3>
         <a
-          href="https://brandononeal.medium.com/designing-a-homeless-shelter-management-system-5213636b8f40"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <RocketOutlined /> Medium Article{" "}
-        </a>
-        <a
           href="https://github.com/brandononeal/family-promise-spokane-fe-a"
           target="_blank"
           rel="noreferrer"
         >
-          <GithubOutlined /> Link to GitHub
+          <GithubOutlined /> GitHub Repo{" "}
+        </a>
+        <a
+          href="https://brandononeal.medium.com/designing-a-homeless-shelter-management-system-5213636b8f40"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <RocketOutlined /> Medium Article
         </a>
         <ul>
           <li>
@@ -54,18 +54,18 @@ function Projects() {
         <h2>Family Promise: Service Tracker</h2>
         <h3>Backend Engineer - Node.js</h3>
         <a
-          href="https://brandononeal.medium.com/developing-a-service-tracker-for-family-promise-742a93cdb3f4"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <RocketOutlined /> Medium Article{" "}
-        </a>
-        <a
           href="https://github.com/brandononeal/family-promise-service-tracker-be-a"
           target="_blank"
           rel="noreferrer"
         >
-          <GithubOutlined /> Link to GitHub
+          <GithubOutlined /> GitHub Repo{" "}
+        </a>
+        <a
+          href="https://brandononeal.medium.com/developing-a-service-tracker-for-family-promise-742a93cdb3f4"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <RocketOutlined /> Medium Article
         </a>
         <ul>
           <li>
@@ -95,18 +95,18 @@ function Projects() {
         <h2>Anywhere Fitness</h2>
         <h3>UI Designer - HTML/CSS</h3>
         <a
-          href="https://build-wk-anywhere-fitness-3.github.io/ui/index.html"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <RocketOutlined /> Deployed Application{" "}
-        </a>
-        <a
           href="https://github.com/Build-Wk-Anywhere-Fitness-3/ui"
           target="_blank"
           rel="noreferrer"
         >
-          <GithubOutlined /> Link to GitHub
+          <GithubOutlined /> GitHub Repo{" "}
+        </a>
+        <a
+          href="https://build-wk-anywhere-fitness-3.github.io/ui/index.html"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <RocketOutlined /> Deployed Application
         </a>
         <ul>
           <li>
@@ -132,18 +132,18 @@ function Projects() {
         <h2>Water My Plants</h2>
         <h3>Frontend Engineer - React</h3>
         <a
-          href="https://water-my-plants-mu.vercel.app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <RocketOutlined /> Deployed Application{" "}
-        </a>
-        <a
           href="https://github.com/Build-Wk-Water-My-Plants/front-end"
           target="_blank"
           rel="noreferrer"
         >
-          <GithubOutlined /> Link to GitHub
+          <GithubOutlined /> GitHub Repo{" "}
+        </a>
+        <a
+          href="https://water-my-plants-mu.vercel.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <RocketOutlined /> Deployed Application
         </a>
       </div>
       <br />
